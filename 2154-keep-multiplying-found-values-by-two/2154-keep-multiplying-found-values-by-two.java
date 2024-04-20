9870d934916e59b3f55d2f9b4b177411e61cb268
anirudharkery <48996209+anirudharkery@users.noreply.github.com>
2024-04-20 13:57:57 -0700

    Time: 3 ms (60.47%), Space: 43.9 MB (63.79%) - LeetHub

--- 2154-keep-multiplying-found-values-by-two/2154-keep-multiplying-found-values-by-two.java
+++ 2154-keep-multiplying-found-values-by-two/2154-keep-multiplying-found-values-by-two.java
@@ -1,23 +1,32 @@
 class Solution {
     public int findFinalValue(int[] nums, int original) {
-        Arrays.sort(nums);
-        int val=original;
-        Map<Integer,Integer> mp = new HashMap<>();
+//         Arrays.sort(nums);
+//         int val=original;
+//         Map<Integer,Integer> mp = new HashMap<>();
         
             
-        for(int i=0;i<nums.length;i++)
-        {
-            mp.put(nums[i],mp.getOrDefault(nums[i],0)+1);
-        }
+//         for(int i=0;i<nums.length;i++)
+//         {
+//             mp.put(nums[i],mp.getOrDefault(nums[i],0)+1);
+//         }
         
-        for(int i=0;i<nums.length;i++)
-        {
-            if(mp.containsKey(val))
-            {
-                val = val*2;
-            }
-        }
-        return val;
+//         for(int i=0;i<nums.length;i++)
+//         {
+//             if(mp.containsKey(val))
+//             {
+//                 val = val*2;
+//             }
+//         }
+//         return val;
+        HashSet<Integer> has= new HashSet<>(nums.length);
+for (int i=0;i<=nums.length-1;i++)
+	has.add(nums[i]);
+for (int i=0;i<=nums.length-1;i++) {
+	if (!has.contains(original))
+		break;
+	original = original * 2;
+}
+return original; 
         
     }
 }
