870bf5300dac4848ff47fde5a265794825c09b9b
anirudharkery <48996209+anirudharkery@users.noreply.github.com>
2024-04-19 23:18:56 -0700

    Time: 4 ms (93.25%), Space: 43.8 MB (33.20%) - LeetHub

--- 2006-count-number-of-pairs-with-absolute-difference-k/2006-count-number-of-pairs-with-absolute-difference-k.java
+++ 2006-count-number-of-pairs-with-absolute-difference-k/2006-count-number-of-pairs-with-absolute-difference-k.java
@@ -1,43 +1,24 @@
-// class Solution {
-//     public int countKDifference(int[] nums, int k) {
-        
-//         Map<Integer,Integer> mp = new HashMap<>();
-        
-//         int res=0;
-        
-//         for(int i=0;i<nums.length;i++)
-//         {
-//             if(mp.containsKey(nums[i]+k))
-//             {
-//                 res +=  mp.get(nums[i]+k);
-//             }
-//             else if(mp.containsKey(nums[i]-k))
-//             {
-//                 res +=  mp.get(nums[i]-k);
-//             }
-//             mp.put(nums[i],mp.getOrDefault(nums[i],0)+1);
-//         }
-        
-//         return res;
-        
-//     }
-// }
 class Solution {
     public int countKDifference(int[] nums, int k) {
-        Map<Integer,Integer> map = new HashMap<>();
-        int res = 0;
         
-        for(int i = 0;i< nums.length;i++){
-            if(map.containsKey(nums[i]-k)){
-                res+= map.get(nums[i]-k);
+        Map<Integer,Integer> mp = new HashMap<>();
+        
+        int res=0;
+        
+        for(int i=0;i<nums.length;i++)
+        {
+            if(mp.containsKey(nums[i]+k))
+            {
+                res +=  mp.get(nums[i]+k);
             }
-            if(map.containsKey(nums[i]+k)){
-                res+= map.get(nums[i]+k);
+            if(mp.containsKey(nums[i]-k))
+            {
+                res +=  mp.get(nums[i]-k);
             }
-            map.put(nums[i],map.getOrDefault(nums[i],0)+1);
+            mp.put(nums[i],mp.getOrDefault(nums[i],0)+1);
         }
         
-        
         return res;
+        
     }
 }
